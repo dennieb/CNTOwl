3c6e5152528f3ec2642d41886c5715caa24854b0
dennieb <dennieford@hotmail.com>
2018-05-31 11:02:55 +0200

    added twitter and help command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ import java.util.Properties;
 public class Main extends ListenerAdapter {
 
     public String prefix = "!";
-    public DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    public DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
     public Date date = new Date();
 
     public static void main(String[] args) throws LoginException, IOException {
@@ -54,13 +54,31 @@ public class Main extends ListenerAdapter {
                 event.getAuthor().getName() + ": " +
                 event.getMessage().getContentDisplay()
         );
+
         //Change .equals("String") to trigger word
         if (event.getMessage().getContentRaw().equals(prefix + "vote")) {
         //Message being shown after keyword in sendMessage("String").queue()
             event.getChannel().sendMessage("Kah Kah! It is " + dateFormat.format(date) +
                     ". Seems like a perfect time to vote! " +
-                    "https://clanlist.io/vote/CNTO").queue();
+                    "https://clanlist.io/vote/CNTO" + " and upvote on Reddit! " +
+                    "https://www.reddit.com/r/FindAUnit/").queue();
         }
+
+        if (event.getMessage().getContentRaw().equals(prefix + "twitter")) {
+            event.getChannel().sendMessage("Twitter is being managed by Shails!" + System.lineSeparator() +
+                    "https://twitter.com/carpenoctemarma"  +
+                    " check it out! Kah Kah!").queue();
+        }
+
+        if (event.getMessage().getContentRaw().equals(prefix + "help")) {
+            event.getChannel().sendMessage
+                    (
+                    prefix + "vote " + System.lineSeparator() + "*Shows the voting links*" + System.lineSeparator() + "" + System.lineSeparator() +
+                         prefix + "twitter " + System.lineSeparator() + "*Shows the CNTO twitter channel*" + System.lineSeparator() + "" + System.lineSeparator()
+                    ).queue();
+        }
+
+
         //Ignore if message comes from another bot.
         if (event.getAuthor().isBot()) {
             return;
